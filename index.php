50eedfe262912064f8640366afce34c222340893
kovalj1337 <vanaplayers@gmail.com>
2024-04-15 17:52:16 +0300

    dd

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <div class="header display-flex">
@@ -18,10 +20,16 @@
             <div class="cabinet-mulyazh">
                 <a href="#">Мій профіль(муляж)</a>
             </div>
-            </div>
-        </header>
-        <main class="display-flex">
-            
+        </div>
+    </header>
+    <main class="">
+        <div class="display-flex logotipe">
+            <img src="image/logo-reshka.png" alt="" width="300px" height="150px">
+        </div>
+        <div class="display-flex logotipe">
+            <img src="image/mode.png" alt="" width="200px" height="75px">
+        </div>
+        <div class="display-flex mode">
             <?php
             if (isset($_POST["winstreak"])) {
                 $winstreak = $_POST["winstreak"];
@@ -30,24 +38,31 @@
             }
             ?>
             <form method="post" action="game.php">
-                <input type="hidden" name="winstreak" value="<?php echo ($winstreak);?>">
+                <input type="hidden" name="winstreak" value="<?php echo ($winstreak); ?>">
                 <button type="submit" name="mode" value="easy" class="techies-easy"></button>
-                <button type="submit" name="mode" value="mid" class="techies-mid" <?php 
-                if($winstreak >= 2){
+                <button type="submit" name="mode" value="mid" class="techies-mid" <?php
+                if ($winstreak >= 2) {
                     echo "";
-                }else{
+                } else {
                     echo ("disabled");
                 }
                 ?>>Середній</button>
-                <button type="submit" name="mode" value="hard" class="techies-hard" <?php 
-                if($winstreak >= 3){
+                <button type="submit" name="mode" value="hard" class="techies-hard" <?php
+                if ($winstreak >= 3) {
                     echo "";
-                }else{
+                } else {
                     echo ("disabled");
                 }
                 ?>>Важкий</button>
                 <input type="hidden" name="attempts" value="0">
             </form>
-        </main>
+        </div>
+    </main>
+    <footer>
+        <div class="footer">
+            <img src="image/reklama.png" alt="" width="1280px" height="75px">
+        </div>
+    </footer>
 </body>
+
 </html>
