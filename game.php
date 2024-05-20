adc7bcb50ba80daf358671160e30202c1ad160b7
kovalj1337 <vanaplayers@gmail.com>
2024-05-20 19:56:18 +0400

    lol

--- game.php
+++ game.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="style.css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <header>
+        <div class="header display-flex">
+            <div>
+                <img src="image/logo.png" alt="">
+            </div>
+            <div>
+                <img src="image/techies-header.jpg" alt="" width="100px" height="50px">
+            </div>
+            <div class="cabinet-mulyazh">
+                <a href="#">Мій профіль(муляж)</a>
+            </div>
+        </div>
+    </header>
     <?php
     if ($_POST) {
         $winstreak = $_POST["winstreak"];
@@ -26,8 +39,8 @@
             if ($attempts <= 10) {
                 switch ($mode) {
                     case 'easy';
-                    echo ("<p>Ваших перемог" . "$wins" . "/" . "10</p>");
-                    echo ("<p>Спроба " . "$attempts" . "/" . "10</p>");
+                        echo ("<p>Ваших перемог" . "$wins" . "/" . "10</p>");
+                        echo ("<p>Спроба " . "$attempts" . "/" . "10</p>");
                         if ($game == $lol) {
                             echo ("Ви виграли");
                             $_POST['attempts'] = ++$attempts;
@@ -45,7 +58,7 @@
                             }
                         }
                         break;
-                        case "mid";
+                    case "mid";
                         echo ("<p>Ваших перемог" . "$wins" . "/" . "10</p>");
                         echo ("<p>Спроба " . "$attempts" . "/" . "10</p>");
                         if ($game == $lol) {
@@ -62,7 +75,7 @@
                             echo ("Ви програли");
                         }
                         break;
-                        case "hard";
+                    case "hard";
                         echo ("<p>Ваших перемог" . "$wins" . "/" . "10</p>");
                         echo ("<p>Спроба " . "$attempts" . "/" . "10</p>");
                         if ($game != $lol) {
@@ -87,30 +100,41 @@
                 echo ("<a href='index.php'>На головну</a>");
             }
         } else {
-            echo ("Виберіть сторону");
+            echo ("<p class='display-flex justify-content-center'>
+            <img src='image/chose-side.png' alt='' width='300px' height='150px'>
+            </p>");
         }
         // var_dump($attempts);
     }
     ?>
-<form id="form" action="
-    <?php
-    if ($attempts == 10) {
-        echo ("results.php");
-    } else {
-        echo ("game.php");
-    }
-    ?>
-    " method="POST">
-    <input type="radio" name="game" value="1" id="orel">
-    <label for="orel">Орел</label>
-    <input type="radio" name="game" value="2" id="reshka">
-    <label for="reshka">Reshka</label>
-    <input type="hidden" name="attempts" value="<?php echo ($attempts); ?>">
-    <input type="hidden" name="mode" value="<?php echo ($mode); ?>">
-    <input type="hidden" name="wins" value="<?php echo ($wins); ?>">
-    <input type="hidden" name="winstreak" value="<?php echo ($winstreak);?>">
-    <button type="submit">sabmit</button>
-</form>
+    <div>
+        
+        <form id="form" action="
+        <?php
+        if ($attempts == 10) {
+            echo ("results.php");
+        } else {
+            echo ("game.php");
+        }
+        ?>
+        " method="POST">
+            <div class="display-flex game">
+                <p class="orel">
+                    <input type="radio" name="game" value="1" id="orel">
+                    <label for="orel"></label>
+                </p>
+                <p class="reshka">
+                    <input type="radio" name="game" value="2" id="reshka">
+                    <label for="reshka"></label>
+                </p>
+            </div>
+            <input type="hidden" name="attempts" value="<?php echo ($attempts); ?>">
+            <input type="hidden" name="mode" value="<?php echo ($mode); ?>">
+            <input type="hidden" name="wins" value="<?php echo ($wins); ?>">
+            <input type="hidden" name="winstreak" value="<?php echo ($winstreak); ?>">
+            <p class="display-flex justify-content-center"><button type="submit" class="submit"></button></p>
+        </form>
+    </div>
 </body>
 
 </html>
